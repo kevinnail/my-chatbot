1c15409343b5c87dd726b68c88a0e325f24752ae
kevinnail <fumalicious@gmail.com>
2025-07-11 13:30:55 -0700

    Refactor ESLint configuration for improved readability and consistency by updating rule syntax and adding missing options.

--- server/eslint.config.js
+++ server/eslint.config.js
@@ -11,27 +11,27 @@ export default [
       globals: {
         ...globals.node,
         ...globals.jest,
-        ...globals.es2021
-      }
+        ...globals.es2021,
+      },
     },
     rules: {
-      'no-console': ['warn', { 'allow': ['info', 'error'] }],
-      'indent': ['error', 2, { 'SwitchCase': 1 }],
-      'quotes': ['error', 'single', { 'avoidEscape': true }],
-      'semi': ['error', 'always'],
+      'no-console': ['warn', { allow: ['info', 'error'] }],
+      indent: ['error', 2, { SwitchCase: 1 }],
+      quotes: ['error', 'single', { avoidEscape: true }],
+      semi: ['error', 'always'],
       'space-in-parens': ['error'],
       'space-infix-ops': 'error',
       'object-curly-spacing': ['error', 'always'],
       'comma-spacing': 'error',
       'eol-last': ['error', 'always'],
-      'arrow-spacing': ['error', { 'before': true, 'after': true }],
+      'arrow-spacing': ['error', { before: true, after: true }],
       'array-bracket-spacing': 'error',
       'prefer-const': 'error',
       'no-var': 'error',
       'rest-spread-spacing': 'error',
       'prefer-arrow-callback': 'error',
       'object-shorthand': ['error', 'always'],
-      'no-unused-vars': ['error']
-    }
-  }
+      'no-unused-vars': ['error', { args: 'none' }],
+    },
+  },
 ];
