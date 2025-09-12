f5fce33ac2fed515b433c920baa0606f56705766
kevinnail <fumalicious@gmail.com>
2025-09-11 21:52:29 -0700

    add loading text to title

--- client/src/components/ChatList/ChatList.js
+++ client/src/components/ChatList/ChatList.js
@@ -178,7 +178,7 @@ const ChatList = ({ userId }) => {
                   ×
                 </button>
               </div>
-              <div className="chat-preview">{chat.title}</div>
+              <div className="chat-preview">{chat?.title ? chat.title : 'title generating...'}</div>
               <div className="chat-meta">
                 {chat.messageCount} message{chat.messageCount !== 1 ? 's' : ''}
               </div>
