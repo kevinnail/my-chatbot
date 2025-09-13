587148986a2c2b4acb73335d532532affdb36b20
kevinnail <fumalicious@gmail.com>
2025-09-12 21:09:12 -0700

    add /users route

--- server/lib/app.js
+++ server/lib/app.js
@@ -5,6 +5,7 @@ import gmailMcpRouter from './controllers/gmail.js';
 import googleCalendarRouter from './controllers/googleCalendar.js';
 import notFound from './middleware/not-found.js';
 import errorHandler from './middleware/error.js';
+import users from './controllers/users.js';
 
 const app = express();
 app.use(
@@ -18,6 +19,7 @@ app.use(
 app.use(express.json());
 
 // Use the imported routers
+app.use('/api/users', users);
 app.use('/api/chatbot', chatRouter);
 app.use('/api/gmail', gmailMcpRouter);
 app.use('/api/calendar', googleCalendarRouter);
