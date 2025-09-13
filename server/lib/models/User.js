4ff046d6ee52620a3ed72d812d32c4f7e27706f1
kevinnail <fumalicious@gmail.com>
2025-09-12 19:35:56 -0700

    Add user authentication and management functionality
    - Implement user registration and session management in users controller.
    - Create authentication middleware to verify user sessions.
    - Develop user model for database interactions, including user creation and retrieval.
    - Introduce UserService for handling user-related business logic, including password hashing and JWT generation.
    - Update SQL setup script to create users table with necessary fields.

---
+++ server/lib/models/User.js
@@ -0,0 +1,66 @@
+const pool = require('../utils/pool');
+const Todo = require('../models/Todo');
+
+module.exports = class User {
+  id;
+  email;
+  todos;
+  #passwordHash; // private class field: hides it from anything outside of this class definition
+
+  constructor(row) {
+    this.id = row.id;
+    this.email = row.email;
+    this.#passwordHash = row.password_hash;
+  }
+
+  static async insert({ email, passwordHash }) {
+    const { rows } = await pool.query(
+      `
+      INSERT INTO users2 ( email, password_hash)
+      VALUES ($1, $2)
+      RETURNING *
+    `,
+      [email, passwordHash]
+    );
+
+    return new User(rows[0]);
+  }
+
+  static async getAll() {
+    const { rows } = await pool.query('SELECT * FROM users2');
+    return rows.map((row) => new User(row));
+  }
+
+  static async getByEmail(email) {
+    const { rows } = await pool.query(
+      `
+      SELECT *
+      FROM users2
+      WHERE email=$1
+      `,
+      [email]
+    );
+
+    if (!rows[0]) return null;
+
+    return new User(rows[0]);
+  }
+
+  get passwordHash() {
+    return this.#passwordHash;
+  }
+
+  async getTodos() {
+    const { rows } = await pool.query(
+      `
+      SELECT * FROM todos5
+      WHERE user_id=$1
+      ORDER BY created_at DESC;
+      
+    `,
+      [this.id]
+    );
+
+    this.todos = rows.map((row) => new Todo(row));
+  }
+};
