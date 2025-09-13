d23eaa1e430b18bcae865463b279a739955c1188
kevinnail <fumalicious@gmail.com>
2025-09-12 22:18:36 -0700

    add cookieparser to app

--- server/lib/app.js
+++ server/lib/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import chatRouter from './controllers/chat.js';
 import gmailMcpRouter from './controllers/gmail.js';
 import googleCalendarRouter from './controllers/googleCalendar.js';
@@ -18,6 +19,7 @@ app.use(
   }),
 );
 app.use(express.json());
+app.use(cookieParser());
 
 // Use the imported routers
 app.use('/api/users', users);
