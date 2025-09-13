acc010461f4438ee67333c4c9545b4311703dbfb
kevinnail <fumalicious@gmail.com>
2025-09-12 23:45:32 -0700

    add user tests

---
+++ server/__tests__/controllers/users.test.js
@@ -0,0 +1,73 @@
+import { jest, describe, beforeEach, afterAll, it, expect } from '@jest/globals';
+import { pool } from '../../lib/utils/db.js';
+import setup from '../../test-setup.js';
+import request from 'supertest';
+import app from '../../lib/app.js';
+import UserService from '../../lib/services/UserService.js';
+
+// Dummy user for testing
+const mockUser = {
+  firstName: 'Test',
+  lastName: 'User',
+  email: 'test@example.com',
+  password: '12345',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  // Create an "agent" that gives us the ability
+  // to store cookies between requests in a test
+  const agent = request.agent(app);
+
+  // Create a user to sign in with
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  // ...then sign in
+  const { email } = user;
+  await agent.post('/api/users/sessions').send({ email, password });
+  return [agent, user];
+};
+
+describe('user routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  afterAll(() => {
+    pool.end();
+  });
+
+  it('creates a new user', async () => {
+    const res = await request(app).post('/api/users').send(mockUser);
+    const { email } = mockUser;
+    expect(res.body).toEqual({
+      id: expect.any(String),
+      email,
+    });
+  });
+
+  it('signs in an existing user', async () => {
+    await request(app).post('/api/users').send(mockUser);
+    const res = await request(app)
+      .post('/api/users/sessions')
+      .send({ email: 'test@example.com', password: '12345' });
+    expect(res.status).toEqual(200);
+  });
+
+  it('/protected should return a 401 if not authenticated', async () => {
+    const res = await request(app).get('/api/users/protected');
+    expect(res.status).toEqual(401);
+  });
+
+  it('/protected should return the current user if authenticated', async () => {
+    const [agent] = await registerAndLogin();
+    const res = await agent.get('/api/users/protected');
+    expect(res.status).toEqual(200);
+  });
+
+  it('DELETE /sessions deletes the user session', async () => {
+    const [agent] = await registerAndLogin();
+    const resp = await agent.delete('/api/users/sessions');
+    expect(resp.status).toBe(204);
+  });
+});
