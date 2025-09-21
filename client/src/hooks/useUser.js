bb5fb80259f3cd8f528f99d5f118ce73fccc4a8a
kevinnail <fumalicious@gmail.com>
2025-09-20 23:06:43 -0700

    add patch to useUser hook...

--- client/src/hooks/useUser.js
+++ client/src/hooks/useUser.js
@@ -3,12 +3,15 @@ import { UserContext } from '../contexts/UserContext.js';
 import { authUser } from '../services/auth.js';
 
 export function useUser() {
-  const { user, setUser, loading, error, setError, userId, setUserId } = useContext(UserContext);
+  const { user, setUser, loading, setLoading, error, setError, userId, setUserId } =
+    useContext(UserContext);
 
   const logInUser = async (email, password, type) => {
     const user = await authUser(email, password, type);
 
     setUser(user);
+    setUserId(process.env.REACT_APP_GOOGLE_USER_ID);
+    setLoading(false);
   };
 
   return { user, setUser, error, setError, logInUser, loading, userId, setUserId };
