8e84eba756be054f50ea7ed45cce0e8bf172f0da
kevinnail <fumalicious@gmail.com>
2025-10-20 22:20:03 -0700

    remove app.options

--- server/lib/app.js
+++ server/lib/app.js
@@ -19,13 +19,6 @@ app.use(
     allowedHeaders: ['Content-Type', 'Mcp-Session-Id'],
   }),
 );
-app.options(
-  '*',
-  cors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
-    credentials: true,
-  }),
-);
 
 app.use(express.json());
 app.use(cookieParser());
